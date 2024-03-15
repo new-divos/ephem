debd4c1f0d371a79d4477a330e1b6d51c37781d1
Roman A. Voronkin <roman.voronkin@gmail.com>
2024-03-15 18:52:58 +0300

    Added some tests

--- src/core/matrices.rs
+++ src/core/matrices.rs
@@ -2,39 +2,65 @@ use std::{marker::PhantomData, iter::{IntoIterator, Iterator}, ops::{Index, Inde
 
 use crate::core::vectors::{Cartesian, Vec3d};
 
+/// Represents a 3x3 matrix.
 pub struct Mat3d([f64; 9]);
 
 impl Mat3d {
+    /// Constructs a new matrix filled with zeros.
     #[inline]
     pub fn zeros() -> Self {
         Self([0.0; 9])
     }
 
+    /// Constructs a new matrix filled with ones.
     #[inline]
     pub fn ones() -> Self {
         Self([1.0; 9])
     }
 
+    /// Constructs a new identity matrix.
+    ///
+    /// The identity matrix is a square matrix with ones on the main diagonal
+    /// and zeros elsewhere.
     #[inline]
     pub fn eye() -> Self {
         Self([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0])
     }
 
+    /// Constructs a rotation matrix around the x-axis.
+    ///
+    /// # Arguments
+    ///
+    /// * `angle` - The rotation angle in radians.
     pub fn r_x(angle: f64) -> Self {
         let (s, c) = angle.sin_cos();
         Self([1.0, 0.0, 0.0, 0.0, c, s, 0.0, -s, c])
     }
 
+    /// Constructs a rotation matrix around the y-axis.
+    ///
+    /// # Arguments
+    ///
+    /// * `angle` - The rotation angle in radians.
     pub fn r_y(angle: f64) -> Self {
         let (s, c) = angle.sin_cos();
         Self([c, 0.0, -s, 0.0, 1.0, 0.0, s, 0.0, c])
     }
 
+    /// Constructs a rotation matrix around the z-axis.
+    ///
+    /// # Arguments
+    ///
+    /// * `angle` - The rotation angle in radians.
     pub fn f_z(angle: f64) -> Self {
         let (s, c) = angle.sin_cos();
         Self([c, s, 0.0, -s, c, 0.0, 0.0, 0.0, 1.0])
     }
 
+    /// Constructs a matrix from row vectors.
+    ///
+    /// Constructs a matrix using three row vectors. The row vectors represent
+    /// the rows of the resulting matrix.
     #[inline]
     #[rustfmt::skip]
     pub fn with_rows(
@@ -49,6 +75,10 @@ impl Mat3d {
         ])
     }
 
+    /// Constructs a matrix from column vectors.
+    ///
+    /// Constructs a matrix using three column vectors. The column vectors represent
+    /// the columns of the resulting matrix.
     #[inline]
     #[rustfmt::skip]
     pub fn with_columns(
@@ -63,6 +93,33 @@ impl Mat3d {
         ])
     }
 
+    /// Returns a view of the matrix as rows.
+    ///
+    /// This method constructs and returns a `Mat3dView`, which provides a view of the matrix where each row is represented by a `Vec3d<Cartesian>` vector.
+    ///
+    /// # Returns
+    ///
+    /// A `Mat3dView` containing three `Vec3d<Cartesian>` vectors, each representing a row of the matrix.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ephem::core::{
+    ///     vectors::{Cartesian, CartesianBuilder, Vec3d},
+    ///     matrices::Mat3d,
+    /// };
+    /// 
+    /// let row1 = CartesianBuilder::with(1.0, 2.0, 3.0).build();
+    /// let row2 = CartesianBuilder::with(4.0, 5.0, 6.0).build();
+    /// let row3 = CartesianBuilder::with(7.0, 8.0, 9.0).build();
+    ///
+    /// let mat = Mat3d::with_rows(row1, row2, row3);
+    /// let rows = mat.rows();
+    ///
+    /// assert_eq!(rows[0].x(), 1.0);
+    /// assert_eq!(rows[1].y(), 5.0);
+    /// assert_eq!(rows[2].z(), 9.0);
+    /// ```
     #[inline]
     pub fn rows(&self) -> Mat3dView {
         Mat3dView(
@@ -83,6 +140,33 @@ impl Mat3d {
         )
     }
 
+    /// Returns a view of the matrix as columns.
+    ///
+    /// This method constructs and returns a `Mat3dView`, which provides a view of the matrix where each column is represented by a `Vec3d<Cartesian>` vector.
+    ///
+    /// # Returns
+    ///
+    /// A `Mat3dView` containing three `Vec3d<Cartesian>` vectors, each representing a column of the matrix.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ephem::core::{
+    ///     vectors::{Cartesian, CartesianBuilder, Vec3d},
+    ///     matrices::Mat3d,
+    /// };
+    /// 
+    /// let col1 = CartesianBuilder::with(1.0, 2.0, 3.0).build();
+    /// let col2 = CartesianBuilder::with(4.0, 5.0, 6.0).build();
+    /// let col3 = CartesianBuilder::with(7.0, 8.0, 9.0).build();
+    ///
+    /// let mat = Mat3d::with_columns(col1, col2, col3);
+    /// let columns = mat.columns();
+    ///
+    /// assert_eq!(columns[0].x(), 1.0);
+    /// assert_eq!(columns[1].y(), 5.0);
+    /// assert_eq!(columns[2].z(), 9.0);
+    /// ```
     #[inline]
     pub fn columns(&self) -> Mat3dView {
         Mat3dView(
@@ -104,9 +188,22 @@ impl Mat3d {
     }
 }
 
+/// Implements indexing for accessing elements of the `Mat3d` matrix using `(row, column)` tuples.
+///
+/// This allows accessing individual elements of the matrix using zero-based row and column indices.
+/// The returned reference points to the element at the specified row and column.
 impl Index<(usize, usize)> for Mat3d {
     type Output = f64;
 
+    /// Returns a reference to the element at the specified `(row, column)` indices.
+    ///
+    /// # Arguments
+    ///
+    /// * `index` - A tuple representing the zero-based row and column indices.
+    ///
+    /// # Returns
+    ///
+    /// A reference to the element at the specified row and column.
     fn index(&self, index: (usize, usize)) -> &Self::Output {
         let (row, col) = index;
         let index = 3 * row + col;
@@ -115,7 +212,20 @@ impl Index<(usize, usize)> for Mat3d {
     }
 }
 
+/// Implements mutable indexing for accessing and modifying elements of the `Mat3d` matrix using `(row, column)` tuples.
+///
+/// This allows accessing and modifying individual elements of the matrix using zero-based row and column indices.
+/// The returned mutable reference allows modification of the element at the specified row and column.
 impl IndexMut<(usize, usize)> for Mat3d {
+    /// Returns a mutable reference to the element at the specified `(row, column)` indices.
+    ///
+    /// # Arguments
+    ///
+    /// * `index` - A tuple representing the zero-based row and column indices.
+    ///
+    /// # Returns
+    ///
+    /// A mutable reference to the element at the specified row and column.
     fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
         let (row, col) = index;
         let index = 3 * row + col;
@@ -124,9 +234,16 @@ impl IndexMut<(usize, usize)> for Mat3d {
     }
 }
 
+/// Represents a view into a 3x3 matrix (`Mat3d`) consisting of three Cartesian vectors.
+///
+/// This structure provides methods and implementations to interact with the matrix view.
 pub struct Mat3dView([Vec3d<Cartesian>; 3]);
 
 impl Mat3dView {
+    /// Returns an iterator over the rows of the matrix view.
+    ///
+    /// This method constructs and returns an iterator over the rows of the matrix view,
+    /// allowing iteration through each row.
     #[inline]
     pub fn iter(&self) -> Mat3dViewIter<'_> {
         Mat3dViewIter {
@@ -136,55 +253,101 @@ impl Mat3dView {
     }
 }
 
+/// Implements the trait for treating `Mat3dView` as a reference to an array of `Vec3d<Cartesian>` vectors.
+///
+/// This implementation allows treating `Mat3dView` as a reference to an array of Cartesian vectors,
+/// enabling operations and methods that accept slice references.
 impl AsRef<[Vec3d<Cartesian>]> for Mat3dView {
+    /// Returns a reference to the underlying array of Cartesian vectors.
+    ///
+    /// # Returns
+    ///
+    /// A reference to the array of Cartesian vectors contained within the `Mat3dView`.
     #[inline]
     fn as_ref(&self) -> &[Vec3d<Cartesian>] {
         &self.0
     }
 }
 
+/// Implements indexing for accessing individual rows of the `Mat3dView` matrix.
+///
+/// This implementation enables accessing individual rows of the matrix view using zero-based indexing.
 impl Index<usize> for Mat3dView {
     type Output = Vec3d<Cartesian>;
 
+    /// Returns a reference to the row at the specified index.
+    ///
+    /// # Arguments
+    ///
+    /// * `index` - The zero-based index indicating the row to access.
+    ///
+    /// # Returns
+    ///
+    /// A reference to the Cartesian vector representing the row at the specified index.
     #[inline]
     fn index(&self, index: usize) -> &Self::Output {
         &self.0[index]
     }
 }
 
+/// Implements conversion into an iterator for consuming a `Mat3dView`, yielding `Vec3d<Cartesian>` vectors.
+///
+/// This implementation converts a `Mat3dView` into an iterator, consuming the view in the process.
+/// It yields `Vec3d<Cartesian>` vectors as it iterates through each row of the matrix view.
 impl IntoIterator for Mat3dView {
+    /// The type of the items yielded by the iterator, which is a `Vec3d<Cartesian>` vector.
     type Item = Vec3d<Cartesian>;
+    /// The type of the iterator produced by the conversion, which is a `Mat3dViewIntoIter`.
     type IntoIter = Mat3dViewIntoIter;
 
+    /// Consumes the `Mat3dView`, returning an iterator over its rows.
+    ///
+    /// Each row of the matrix view is yielded as a `Vec3d<Cartesian>` vector.
     fn into_iter(self) -> Self::IntoIter {
+        // Reverse the rows to maintain the correct order during iteration
         let mut data = self.0;
         data.reverse();
 
-        Mat3dViewIntoIter {
-            data: data.into_iter().collect(),
-            cursor: 0,
-        }
+        Mat3dViewIntoIter(data.into_iter().collect())
     }
 }
 
+/// Implements conversion into an iterator for borrowing a reference to a `Mat3dView`, yielding shared references to `Vec3d<Cartesian>` vectors.
+///
+/// This implementation converts a reference to a `Mat3dView` into an iterator, allowing iteration over its rows while retaining ownership of the view.
+/// It yields shared references to `Vec3d<Cartesian>` vectors as it iterates through each row of the matrix view.
 impl<'a> IntoIterator for &'a Mat3dView {
+    /// The type of the items yielded by the iterator, which is a shared reference to `Vec3d<Cartesian>` vector.
     type Item = &'a Vec3d<Cartesian>;
+    /// The type of the iterator produced by the conversion, which is a `Mat3dViewIter`.
     type IntoIter = Mat3dViewIter<'a>;
 
+    /// Borrows the `Mat3dView` and returns an iterator over its rows.
+    ///
+    /// Each row of the matrix view is yielded as a shared reference to a `Vec3d<Cartesian>` vector.
     #[inline]
     fn into_iter(self) -> Self::IntoIter {
         self.iter()
     }
 }
 
+/// Iterator over the rows of a `Mat3dView`.
+///
+/// This iterator allows iterating over the rows of a `Mat3dView`, providing references to each row's `Vec3d<Cartesian>` vector.
 pub struct Mat3dViewIter<'a> {
+    /// Reference to the underlying array of `Vec3d<Cartesian>` vectors.
     data: &'a [Vec3d<Cartesian>],
+    /// Cursor indicating the current position within the array.
     cursor: usize,
 }
 
 impl<'a> Iterator for Mat3dViewIter<'a> {
+    /// The type of the items yielded by the iterator, which is a reference to a `Vec3d<Cartesian>` vector.
     type Item = &'a Vec3d<Cartesian>;
 
+    /// Advances the iterator and returns the next row of the matrix view.
+    ///
+    /// Returns `Some(reference)` to the next row if available, otherwise `None`.
     fn next(&mut self) -> Option<Self::Item> {
         if (0..3).contains(&self.cursor) {
             let i = self.cursor;
@@ -197,18 +360,21 @@ impl<'a> Iterator for Mat3dViewIter<'a> {
     }
 }
 
-pub struct Mat3dViewIntoIter {
-    data: Vec<Vec3d<Cartesian>>,
-    cursor: usize
-}
+/// Iterator that consumes a `Mat3dView`, yielding `Vec3d<Cartesian>` vectors.
+///
+/// This iterator consumes a `Mat3dView`, yielding `Vec3d<Cartesian>` vectors as it iterates through each row of the matrix view.
+pub struct Mat3dViewIntoIter(Vec<Vec3d<Cartesian>>);
 
 impl Iterator for Mat3dViewIntoIter {
+    /// The type of the items yielded by the iterator, which is a `Vec3d<Cartesian>` vector.
     type Item = Vec3d<Cartesian>;
 
+    /// Advances the iterator and returns the next `Vec3d<Cartesian>` vector.
+    ///
+    /// Returns `Some(vector)` if a vector is available, otherwise `None`.
     fn next(&mut self) -> Option<Self::Item> {
-        if (0..3).contains(&self.cursor) {
-            self.cursor += 1;
-            self.data.pop()
+        if !self.0.is_empty() {
+            self.0.pop()
         } else {
             None
         }
@@ -219,18 +385,25 @@ impl Iterator for Mat3dViewIntoIter {
 mod tests {
     use super::*;
 
+    /// Test function for Mat3d creation methods.
+    ///
+    /// This test function verifies the correctness of the `zeros()`, `ones()`, and `eye()` methods of the `Mat3d` struct.
+    /// It iterates over each element of the matrices created by these methods and asserts that they have the expected values.
     #[test]
     fn mat3d_creation_test() {
+        // Test zeros matrix creation
         let z = Mat3d::zeros();
         for value in z.0 {
             assert_eq!(value, 0.0);
         }
 
+        // Test ones matrix creation
         let o = Mat3d::ones();
         for value in o.0 {
             assert_eq!(value, 1.0);
         }
 
+        // Test identity matrix creation
         let e = Mat3d::eye();
         for (i, value) in e.0.iter().enumerate() {
             assert_eq!(*value, if i / 3 == i % 3 { 1.0 } else { 0.0 });
--- tests/matrices_tests.rs
+++ tests/matrices_tests.rs
@@ -4,8 +4,10 @@ use ephem::core::{matrices::*, vectors::CartesianBuilder};
 
 mod shared;
 
+/// Test function for trivial Mat3d creation methods.
 #[test]
 fn trivial_mat3d_creation_test() {
+    // Test zeros matrix creation
     let z = Mat3d::zeros();
     for i in 0..3 {
         for j in 0..3 {
@@ -13,6 +15,7 @@ fn trivial_mat3d_creation_test() {
         }
     }
 
+    // Test ones matrix creation
     let o = Mat3d::ones();
     for i in 0..3 {
         for j in 0..3 {
@@ -20,6 +23,7 @@ fn trivial_mat3d_creation_test() {
         }
     }
 
+    // Test identity matrix creation
     let e = Mat3d::eye();
     for i in 0..3 {
         for j in 0..3 {
@@ -28,11 +32,13 @@ fn trivial_mat3d_creation_test() {
     }
 }
 
+/// Test function for Mat3d creation with rows.
 #[test]
 fn mat3d_creation_with_rows_test() {
     let mut rng = rand::thread_rng();
 
     for _ in 0..shared::ITERATIONS {
+        // Generate random Cartesian vectors for each row
         let (x1, y1, z1) = shared::gen_cartesian(&mut rng);
         let row1 = CartesianBuilder::with(x1, y1, z1).build();
 
@@ -42,8 +48,10 @@ fn mat3d_creation_with_rows_test() {
         let (x3, y3, z3) = shared::gen_cartesian(&mut rng);
         let row3 = CartesianBuilder::with(x3, y3, z3).build();
 
+        // Construct a Mat3d using the generated rows
         let m = Mat3d::with_rows(row1, row2, row3);
 
+         // Assert that each element of the resulting matrix corresponds to the correct Cartesian component
         assert_eq!(m[(0, 0)], x1);
         assert_eq!(m[(0, 1)], y1);
         assert_eq!(m[(0, 2)], z1);
@@ -58,11 +66,13 @@ fn mat3d_creation_with_rows_test() {
     }
 }
 
+/// Test function for Mat3d creation with columns.
 #[test]
 fn mat3d_creation_with_columns_test() {
     let mut rng = rand::thread_rng();
 
     for _ in 0..shared::ITERATIONS {
+        // Generate random Cartesian vectors for each column
         let (x1, y1, z1) = shared::gen_cartesian(&mut rng);
         let col1 = CartesianBuilder::with(x1, y1, z1).build();
 
@@ -72,8 +82,10 @@ fn mat3d_creation_with_columns_test() {
         let (x3, y3, z3) = shared::gen_cartesian(&mut rng);
         let col3 = CartesianBuilder::with(x3, y3, z3).build();
 
+        // Construct a Mat3d using the generated columns
         let m = Mat3d::with_columns(col1, col2, col3);
 
+        // Assert that each element of the resulting matrix corresponds to the correct Cartesian component
         assert_eq!(m[(0, 0)], x1);
         assert_eq!(m[(0, 1)], x2);
         assert_eq!(m[(0, 2)], x3);
@@ -85,5 +97,283 @@ fn mat3d_creation_with_columns_test() {
         assert_eq!(m[(2, 0)], z1);
         assert_eq!(m[(2, 1)], z2);
         assert_eq!(m[(2, 2)], z3);
+    }
+}
+
+/// Test function for indexing Mat3d rows through a view.
+#[test]
+fn mat3d_rows_view_index_test() {
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..shared::ITERATIONS {
+        // Generate random Cartesian vectors for each row
+        let (x1, y1, z1) = shared::gen_cartesian(&mut rng);
+        let row1 = CartesianBuilder::with(x1, y1, z1).build();
+
+        let (x2, y2, z2) = shared::gen_cartesian(&mut rng);
+        let row2 = CartesianBuilder::with(x2, y2, z2).build();
+
+        let (x3, y3, z3) = shared::gen_cartesian(&mut rng);
+        let row3 = CartesianBuilder::with(x3, y3, z3).build();
+
+        // Construct a Mat3d using the generated rows
+        let m = Mat3d::with_rows(row1, row2, row3);
+        let rows = m.rows();
+
+        // Assert that each element of the resulting view corresponds to the correct Cartesian component
+        assert_eq!(rows[0].x(), x1);
+        assert_eq!(rows[0].y(), y1);
+        assert_eq!(rows[0].z(), z1);
+
+        assert_eq!(rows[1].x(), x2);
+        assert_eq!(rows[1].y(), y2);
+        assert_eq!(rows[1].z(), z2);
+
+        assert_eq!(rows[2].x(), x3);
+        assert_eq!(rows[2].y(), y3);
+        assert_eq!(rows[2].z(), z3);
+    }
+}
+
+/// Test function for iterating over rows of a Mat3d.
+#[test]
+fn mat3d_rows_view_iterator_test() {
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..shared::ITERATIONS {
+        // Generate random Cartesian vectors for each row
+        let (x1, y1, z1) = shared::gen_cartesian(&mut rng);
+        let row1 = CartesianBuilder::with(x1, y1, z1).build();
+
+        let (x2, y2, z2) = shared::gen_cartesian(&mut rng);
+        let row2 = CartesianBuilder::with(x2, y2, z2).build();
+
+        let (x3, y3, z3) = shared::gen_cartesian(&mut rng);
+        let row3 = CartesianBuilder::with(x3, y3, z3).build();
+
+        // Construct a Mat3d using the generated rows
+        let m = Mat3d::with_rows(row1, row2, row3);
+        let rows = m.rows();
+
+        // Obtain an iterator from the view and iterate over the rows
+        let mut rows_iter = rows.iter();
+
+        let mut value = rows_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x1);
+            assert_eq!(vector.y(), y1);
+            assert_eq!(vector.z(), z1);
+        }
+
+        value = rows_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x2);
+            assert_eq!(vector.y(), y2);
+            assert_eq!(vector.z(), z2);
+        }
+
+        value = rows_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x3);
+            assert_eq!(vector.y(), y3);
+            assert_eq!(vector.z(), z3);
+        }
+
+        value = rows_iter.next();
+        assert!(value.is_none());
+    }
+}
+
+/// Test function for iterating over rows of a Mat3d using into_iter.
+#[test]
+fn mat3d_rows_view_into_iterator_test() {
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..shared::ITERATIONS {
+        // Generate random Cartesian vectors for each row
+        let (x1, y1, z1) = shared::gen_cartesian(&mut rng);
+        let row1 = CartesianBuilder::with(x1, y1, z1).build();
+
+        let (x2, y2, z2) = shared::gen_cartesian(&mut rng);
+        let row2 = CartesianBuilder::with(x2, y2, z2).build();
+
+        let (x3, y3, z3) = shared::gen_cartesian(&mut rng);
+        let row3 = CartesianBuilder::with(x3, y3, z3).build();
+
+        // Construct a Mat3d using the generated rows
+        let m = Mat3d::with_rows(row1, row2, row3);
+        let rows = m.rows();
+
+        // Obtain an iterator from the view and iterate over the rows
+        let mut rows_iter = rows.into_iter();
+
+        let mut value = rows_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x1);
+            assert_eq!(vector.y(), y1);
+            assert_eq!(vector.z(), z1);
+        }
+
+        value = rows_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x2);
+            assert_eq!(vector.y(), y2);
+            assert_eq!(vector.z(), z2);
+        }
+
+        value = rows_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x3);
+            assert_eq!(vector.y(), y3);
+            assert_eq!(vector.z(), z3);
+        }
+
+        value = rows_iter.next();
+        assert!(value.is_none());
+    }
+}
+
+/// Test function for accessing columns of a Mat3d using index.
+#[test]
+fn mat3d_columns_view_index_test() {
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..shared::ITERATIONS {
+        // Generate random Cartesian vectors for each column
+        let (x1, y1, z1) = shared::gen_cartesian(&mut rng);
+        let col1 = CartesianBuilder::with(x1, y1, z1).build();
+
+        let (x2, y2, z2) = shared::gen_cartesian(&mut rng);
+        let col2 = CartesianBuilder::with(x2, y2, z2).build();
+
+        let (x3, y3, z3) = shared::gen_cartesian(&mut rng);
+        let col3 = CartesianBuilder::with(x3, y3, z3).build();
+
+        // Construct a Mat3d using the generated columns
+        let m = Mat3d::with_columns(col1, col2, col3);
+        let columns = m.columns();
+
+        // Access the columns using index operations and assert the vector components
+        assert_eq!(columns[0].x(), x1);
+        assert_eq!(columns[0].y(), y1);
+        assert_eq!(columns[0].z(), z1);
+
+        assert_eq!(columns[1].x(), x2);
+        assert_eq!(columns[1].y(), y2);
+        assert_eq!(columns[1].z(), z2);
+
+        assert_eq!(columns[2].x(), x3);
+        assert_eq!(columns[2].y(), y3);
+        assert_eq!(columns[2].z(), z3);
+    }
+}
+
+/// Test function for iterating over columns of a Mat3d.
+#[test]
+fn mat3d_columns_view_iterator_test() {
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..shared::ITERATIONS {
+        // Generate random Cartesian vectors for each column
+        let (x1, y1, z1) = shared::gen_cartesian(&mut rng);
+        let col1 = CartesianBuilder::with(x1, y1, z1).build();
+
+        let (x2, y2, z2) = shared::gen_cartesian(&mut rng);
+        let col2 = CartesianBuilder::with(x2, y2, z2).build();
+
+        let (x3, y3, z3) = shared::gen_cartesian(&mut rng);
+        let col3 = CartesianBuilder::with(x3, y3, z3).build();
+
+        // Construct a Mat3d using the generated columns
+        let m = Mat3d::with_columns(col1, col2, col3);
+        let columns = m.columns();
+
+        // Create an iterator over the columns and assert the vector components
+        let mut columns_iter = columns.iter();
+
+        let mut value = columns_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x1);
+            assert_eq!(vector.y(), y1);
+            assert_eq!(vector.z(), z1);
+        }
+
+        value = columns_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x2);
+            assert_eq!(vector.y(), y2);
+            assert_eq!(vector.z(), z2);
+        }
+
+        value = columns_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x3);
+            assert_eq!(vector.y(), y3);
+            assert_eq!(vector.z(), z3);
+        }
+
+        value = columns_iter.next();
+        assert!(value.is_none());
+    }
+}
+
+/// Test function for iterating over columns of a Mat3d using into iterator.
+#[test]
+fn mat3d_columns_view_into_iterator_test() {
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..shared::ITERATIONS {
+        // Generate random Cartesian vectors for each column
+        let (x1, y1, z1) = shared::gen_cartesian(&mut rng);
+        let col1 = CartesianBuilder::with(x1, y1, z1).build();
+
+        let (x2, y2, z2) = shared::gen_cartesian(&mut rng);
+        let col2 = CartesianBuilder::with(x2, y2, z2).build();
+
+        let (x3, y3, z3) = shared::gen_cartesian(&mut rng);
+        let col3 = CartesianBuilder::with(x3, y3, z3).build();
+
+        // Construct a Mat3d using the generated columns
+        let m = Mat3d::with_columns(col1, col2, col3);
+        let columns = m.columns();
+
+        // Create an into iterator over the columns and assert the vector components
+        let mut columns_iter = columns.into_iter();
+
+        let mut value = columns_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x1);
+            assert_eq!(vector.y(), y1);
+            assert_eq!(vector.z(), z1);
+        }
+
+        value = columns_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x2);
+            assert_eq!(vector.y(), y2);
+            assert_eq!(vector.z(), z2);
+        }
+
+        value = columns_iter.next();
+        assert!(value.is_some());
+        if let Some(vector) = value {
+            assert_eq!(vector.x(), x3);
+            assert_eq!(vector.y(), y3);
+            assert_eq!(vector.z(), z3);
+        }
+
+        value = columns_iter.next();
+        assert!(value.is_none());
     }
 }
